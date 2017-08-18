cacd4eb66ff4fa58296f2fc0a150304ea610362b
n.yevdokymenko <n.yevdokymenko@aimprosoft.com>
2017-08-18 20:23:52 +0300

    Install and configurate liferay.

---
+++ src/main/java/ua/com/controllers/impl/MainPortlet.java
@@ -0,0 +1,18 @@
+package ua.com.controllers.impl;
+
+import javax.portlet.GenericPortlet;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+import java.io.IOException;
+
+/**
+ * Created on 17.08.17.
+ */
+public class MainPortlet extends GenericPortlet {
+
+    @Override
+    protected void doView(RenderRequest request, RenderResponse response) throws PortletException, IOException {
+
+    }
+}
