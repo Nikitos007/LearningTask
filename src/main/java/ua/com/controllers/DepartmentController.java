c0a818b44d26a58b25eba74db9b53b921afdf8b1
n.yevdokymenko <n.yevdokymenko@aimprosoft.com>
2017-09-22 18:34:17 +0300

    Add oval validation.

--- src/main/java/ua/com/controllers/DepartmentController.java
+++ src/main/java/ua/com/controllers/DepartmentController.java
@@ -10,7 +10,6 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
-import javax.persistence.Entity;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -44,29 +43,33 @@ public class DepartmentController {
     }
 
 
-    public Map<String, String> validateErrors = new HashMap<>();
+    private Map<String, String> validateErrors = new HashMap<>();
+    private Integer sizeOfErrors;
 
-    public Map<String, String> getValidateErrors() {
-        return validateErrors;
+    public Integer getSizeOfErrors() {
+        return sizeOfErrors;
     }
 
     public String getError(String key) {
-       return validateErrors.get(key);
+        String error = validateErrors.get(key);
+        validateErrors.remove(key);
+        return error;
     }
 
-    public void save() {
-        validateErrors.clear();
+    public void save() { sizeOfErrors = 0;
         try {
             departmentService.save(department);
         } catch (ValidateException e) {
+            validateErrors.clear();
             validateErrors = e.getErrorsMap();
-
-            for(Map.Entry<String, String> entry : e.getErrorsMap().entrySet()) {
+            sizeOfErrors = validateErrors.size();
+            for (Map.Entry<String, String> entry : e.getErrorsMap().entrySet()) {
                 FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, entry.getValue(), null));
             }
             return;
         }
         this.department = new Department();
+
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Department saved!", null));
     }
 
